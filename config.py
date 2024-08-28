684889fc68fc3267355de9cb4c06678708baeabe
h-pgy <hpougy@gmail.com>
2024-08-28 16:29:24 -0300

    env variables

---
+++ config.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from typing import Union
+import os
+
+def load_env_var(varname:str)->Union[str, int, float, bool]:
+
+
+    if not os.path.exists('.env'):
+        print('Creating environment from .env.example')
+        with open('.env', 'w') as envfile:
+            with open('.env.example', 'r') as envexample:
+                content = envexample.read()
+                envfile.write(content)
+
+    load_dotenv()
+
+    variable = os.getenv(varname)
+    if variable is None:
+        raise RuntimeError(f'Variavel de ambiente {varname} não definida.')
+    
+    return variable
+
+DOWNLOAD_TTL_SECS = load_env_var('DOWNLOAD_TTL_SECS')
