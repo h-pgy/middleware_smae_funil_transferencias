d433e4bb214b671157af1131228b41b2b9fae61f
h-pgy <hpougy@gmail.com>
2024-08-28 18:34:43 -0300

    str date series to datetime

--- core/utils/datetime.py
+++ core/utils/datetime.py
@@ -1,5 +1,11 @@
 import time
+import pandas as pd
 
 def agora_unix_timestamp():
 
     return int(time.time())
+
+def dmy_series_to_datetime(series:pd.Series)->pd.Series:
+    
+    #erros serao colocados como NaN
+    return pd.to_datetime(series, format=r'%d/%m/%Y', errors='coerce')
